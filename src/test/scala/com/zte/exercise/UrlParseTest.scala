812e0530fa6dfadc905ff23febc7d5997ed2856b
王力朋10165983 <wanglipeng.007@gmail.com>
2014-07-03 15:43:31 +0800

    red, fake implementation can't pass the new TDD test.

--- src/test/scala/com/zte/exercise/UrlParseTest.scala
+++ src/test/scala/com/zte/exercise/UrlParseTest.scala
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith
 class UrlParseTest extends FunSpec with Matchers {
 	describe("UrlParse's get protocol, get domain, get path") {
 		it("http://www.google.com, the protocol should return http," +
-       "the domain should return www.google.com") {
+       "the domain should return www.google.com," +
+       "the path should return empty string") {
 			val url = "http://www.google.com"
 			val urlParse = new UrlParse(url)
 			urlParse.getProtocol() should be ("http")
@@ -16,18 +17,22 @@ class UrlParseTest extends FunSpec with Matchers {
       urlParse.getPath() should be ("")
 		}
     it("ftp://test.com, the protocol should return ftp," +
-      "the domain should return test.com") {
+      "the domain should return test.com," +
+      "the path should return empty string") {
       val url = "ftp://test.com"
       val urlParse = new UrlParse(url)
       urlParse.getProtocol() should be ("ftp")
       urlParse.getDomain() should be ("test.com")
+      urlParse.getPath() should be ("")
     }
     it("https://www.bankOfChina.com/a-path, the protocol should return https," +
-      "the domain should return www.bankOfChina.com") {
+      "the domain should return www.bankOfChina.com," +
+      "the path should return a-path") {
       val url = "https://www.bankOfChina.com/a-path"
       val urlParse = new UrlParse(url)
       urlParse.getProtocol() should be ("https")
       urlParse.getDomain() should be ("www.bankOfChina.com")
+      urlParse.getPath() should be ("a-path")
     }
 	}
 }
