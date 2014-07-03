9f4afd1bea426475018afaec00f96028af7523e3
王力朋10165983 <wanglipeng.007@gmail.com>
2014-07-03 15:37:04 +0800

    refactor the TDD test case to eliminate duplication.

--- src/test/scala/com/zte/exercise/UrlParseTest.scala
+++ src/test/scala/com/zte/exercise/UrlParseTest.scala
@@ -6,38 +6,27 @@ import org.junit.runner.RunWith
 
 @RunWith(classOf[JUnitRunner])
 class UrlParseTest extends FunSpec with Matchers {
-	describe("UrlParse's get protocol") {
-		it("http://www.google.com, the protocol should return http") {
+	describe("UrlParse's get protocol, get domain") {
+		it("http://www.google.com, the protocol should return http," +
+       "the domain should return www.google.com") {
 			val url = "http://www.google.com"
 			val urlParse = new UrlParse(url)
-			urlParse.getProtocol should be ("http")
+			urlParse.getProtocol() should be ("http")
+      urlParse.getDomain() should be ("www.google.com")
 		}
-    it("ftp://test.com, the protocol should return ftp") {
+    it("ftp://test.com, the protocol should return ftp," +
+      "the domain should return test.com") {
       val url = "ftp://test.com"
       val urlParse = new UrlParse(url)
       urlParse.getProtocol() should be ("ftp")
+      urlParse.getDomain() should be ("test.com")
     }
-    it("https://www.bankOfChina.com/a-path, the protocol should return https") {
+    it("https://www.bankOfChina.com/a-path, the protocol should return https," +
+      "the domain should return www.bankOfChina.com") {
       val url = "https://www.bankOfChina.com/a-path"
       val urlParse = new UrlParse(url)
       urlParse.getProtocol() should be ("https")
-    }
-	}
-  describe("UrlParse's get domain") {
-    it("http://www.google.com, the domain should return www.google.com") {
-      val url = "http://www.google.com"
-      val urlParse = new UrlParse(url)
-      urlParse.getDomain() should be ("www.google.com")
-    }
-    it("ftp://test.com, the domain should return test.com") {
-      val url = "ftp://test.com"
-      val urlParse = new UrlParse(url)
-      urlParse.getDomain() should be ("test.com")
-    }
-    it("https://www.bankOfChina.com/a-path, the domain should return www.bankOfChina.com") {
-      val url = "https://www.bankOfChina.com/a-path"
-      val urlParse = new UrlParse(url)
       urlParse.getDomain() should be ("www.bankOfChina.com")
     }
-  }
+	}
 }
